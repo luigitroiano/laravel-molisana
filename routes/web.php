fdc6530667a1d9f621dc6883f620473f48e150b3
LuigiTroiano <troiano.luigi8@gmail.com>
2021-02-12 09:27:09 +0100

    Struttura base Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('product', function () {
+    return view('products');
+});
+
+Route::get('contact-us', function () {
+    return view('contact');
 });
